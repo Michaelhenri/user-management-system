0bb4f6cc441b27954c14f1112849f90515845cc9
Michael Henrique <michaeldt2015@gmail.com>
2025-09-14 14:07:38 -0300

    feat: Estilização completa da interface com React
    Estrutura e estilização completa da interface, utilizando React com JSX e CSS para os componentes e suas funcionalidades

--- app/user-management-system/src/pages/Home/index.jsx
+++ app/user-management-system/src/pages/Home/index.jsx
@@ -3,29 +3,46 @@ import Trash from '../../assets/trash.svg'
 
 function Home() {
 
+  const users = [
+    {
+      id: '1564d5f4fds',
+      name: 'Michael',
+      age: 29,
+      email: 'michael@email.com'
+    },
+    {
+      id: '1564d5156156',
+      name: 'Larissa',
+      age: 32,
+      email: 'larissa@email.com'
+    },
+  ]
   return (
-    
-      <div className='container'>
-        <form>
-          <h1>Cadastro de Usuários</h1>
-          <input name='name' type="text" />
-          <input name='age' type="number" />
-          <input name='email' type="email" />
-          <button type='button'>Cadastrar</button>
-        </form>
 
-        <div>
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder='Nome' name='name' type="text" />
+        <input placeholder='Idade' name='age' type="number" />
+        <input placeholder='E-mail' name='email' type="email" />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className='card'>
           <div>
-            <p>Nome: </p>
-            <p>Idade: </p>
-            <p>Email: </p>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
           </div>
           <button>
             <img src={Trash} alt="Lixeira" />
           </button>
         </div>
-      </div>
-     
+      ))}
+
+    </div>
+
   )
 }
 
